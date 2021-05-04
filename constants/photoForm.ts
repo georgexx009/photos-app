66b16a2c05b95adb3ca507376704ae04ba9c4117
Emmanuel <georgexx009@hotmail.com>
2021-05-03 22:29:30 -0500

    add photo form types and photo form metadata

---
+++ constants/photoForm.ts
@@ -0,0 +1,51 @@
+import {
+  PhotoForm,
+  adjustmentViewOptions,
+  imagePositionOptions,
+  photoMeasureOptions,
+  photoOrientationOptions
+} from '@types'
+
+export const photoFormProperties: PhotoForm[] = [
+  {
+    name: 'name',
+    label: 'Photo name',
+    type: 'text',
+    placeholder: 'photo name'
+  },
+  {
+    name: 'height',
+    label: 'Photo height',
+    type: 'dropdown',
+    defaultValue: '300px',
+    options: (photoMeasureOptions as unknown) as string[]
+  },
+  {
+    name: 'width',
+    label: 'Photo width',
+    type: 'dropdown',
+    defaultValue: '300px',
+    options: (photoMeasureOptions as unknown) as string[]
+  },
+  {
+    name: 'adjustmentView',
+    label: 'Adjustment view',
+    type: 'dropdown',
+    defaultValue: 'height',
+    options: (adjustmentViewOptions as unknown) as string[]
+  },
+  {
+    name: 'imagePosition',
+    label: 'Image position',
+    type: 'dropdown',
+    defaultValue: 'center',
+    options: (imagePositionOptions as unknown) as string[]
+  },
+  {
+    name: 'photoOrientation',
+    label: 'Photo orientation',
+    type: 'dropdown',
+    defaultValue: 'horizontal',
+    options: (photoOrientationOptions as unknown) as string[]
+  }
+];
--- types/photo.ts
+++ types/photo.ts
@@ -1,3 +1,41 @@
 export interface PhotoMetaData {
 	name: string
 }
+
+export const photoOrientationOptions = [
+  'horizontal',
+  'vertical',
+  'square'
+] as const;
+export type PhotoOrientation = typeof photoOrientationOptions[number];
+
+export const adjustmentViewOptions = ['both', 'height', 'width'] as const;
+export type AdjustmentView = typeof adjustmentViewOptions[number];
+
+export const imagePositionOptions = ['center', 'start'] as const;
+export type ImagePosition = typeof imagePositionOptions[number];
+
+export const photoMeasureOptions = [
+  '200px',
+  '250px',
+  '300px',
+  '350px',
+  '400px'
+] as const;
+export type PhotoMeasures = typeof photoMeasureOptions[number];
+
+export interface PhotoForm {
+  name: string;
+  label: string;
+  placeholder?: string;
+  type: 'text' | 'number' | 'dropdown';
+  defaultValue?: string;
+  options?: string[];
+}
+
+export interface Photo {
+  id: string;
+  url: string;
+  name?: string;
+  photoOrientation: PhotoOrientation;
+}
