41f36b28bbaabbed9c4a1e2294dc168405096b45
Emmanuel <georgexx009@hotmail.com>
2021-05-15 15:30:20 -0500

    fix some types

--- lib/cloudinary.ts
+++ lib/cloudinary.ts
@@ -1,6 +1,12 @@
 import cloudinary from 'cloudinary'
 
-cloudinary.config({ 
+interface ICloudinary {
+  config: any
+}
+
+const cloudinaryInstace: ICloudinary = cloudinary as unknown as ICloudinary
+
+cloudinaryInstace.config({ 
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
   api_key: process.env.CLOUDINARY_API_KEY, 
   api_secret: process.env.CLOUDINARY_API_SECRET
