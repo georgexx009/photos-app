13828ee91317a0ba78566fb715649192aeb6c43c
Emmanuel <georgexx009@hotmail.com>
2021-05-17 20:02:04 -0500

    adjust modal height

--- components/Modal/index.tsx
+++ components/Modal/index.tsx
@@ -8,8 +8,8 @@ interface ModalProps {
 
 export const Modal = ({ children, handleCloseModal }: ModalProps) => {
 	return (
-		<div className='column items-center full-screen bg-black bg-opacity-60 top-0 left-0 p-3 lg:p-8 fixed'>
-			<div className='relative my-0 mx-auto w-full md:w-2/3 lg:w-1/2 h-full  bg-white rounded-lg border-gray-300 p-4 lg:p-8'>
+		<div className='grid items-center full-screen bg-black bg-opacity-60 top-0 left-0 p-3 lg:p-8 fixed'>
+			<div className='relative my-0 mx-auto w-full md:w-2/3 lg:w-1/2 h-5/6 lg:h-full  bg-white rounded-lg border-gray-300 p-4 lg:p-8'>
 				{children}
 			</div>
 		</div>
