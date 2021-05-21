71b0892a09c102f6035b1978a1b3ddb8b572adf7
Emmanuel <georgexx009@hotmail.com>
2021-05-20 20:51:59 -0500

    add link to the navbar to log out

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
-import { useSession } from 'next-auth/client'
+import Link from 'next/link'
 
 export const Header = ({ username = 'not logged'}: { username?: string}) => {
 	return (
 		<header className='test p-8 flex justify-between items-center' >
 			<h1>Photo list</h1>
-			<span>{ username }</span>
+			<div>
+				<span className="mr-12">{ username }</span>
+				<Link href="/log-out">
+					<a>Log out</a>
+				</Link>
+			</div>
 		</header>
 	)
 }
