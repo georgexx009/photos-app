b96f4b54c92c18d7c4f81a49983ed068c82ab9e5
georgex009 <jorge.galindo@improving.com>
2021-05-23 14:16:41 -0500

    make header responsive

--- components/Avatar/index.tsx
+++ components/Avatar/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Avatar = ({ imgUrl }: { imgUrl: string }) => {
 	return (
-		<div className="w-16 h-16 relative mb-4">
+		<div className="w-16 h-16 relative">
 			<div className="group w-full h-full rounded-full overflow-hidden shadow-inner text-center bg-purple table cursor-pointer">
 				<span className="hidden group-hover:table-cell text-white font-bold align-middle">KR</span>
 				<img src={imgUrl} alt="lovely avatar" className="object-cover object-center w-full h-full visible group-hover:hidden" />
--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -4,11 +4,11 @@ import { Avatar } from '@components'
 
 export const Header = ({ username = 'not logged', imgUrl }: { username?: string, imgUrl: string }) => {
 	return (
-		<header className='test p-8 flex justify-between items-center' >
+		<header className='p-4 lg:p-8 flex flex-col lg:flex-row justify-between items-center' >
 			<h1>Photo list</h1>
-			<div className="flex justify-items-end items-center">
+			<div className="w-full lg:w-auto flex justify-between lg:justify-end items-center">
 				<Avatar imgUrl={imgUrl} />
-				<span className="mx-12">{ username }</span>
+				<span className="lg:mx-12">{ username }</span>
 				<Link href="/log-out">
 					<a>Log out</a>
 				</Link>
